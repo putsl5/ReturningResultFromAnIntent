740cc026f08686f3c753cf02b748670ec69ff676
Lenovo <putri.salsabila.tif19@polban.ac.id>
2021-07-18 23:08:19 +0700

    Initial commit

---
+++ app/src/main/java/com/android/usingintent2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.android.usingintent2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    int request_Code = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick (View view){
+        startActivityForResult(new Intent("com.android.usingintent." +
+                "SecondActivity"), request_Code);
+    }
+    public void onActivityResult (int requestCode, int resultCode, Intent data){
+        if (requestCode == request_Code){
+            if (resultCode == RESULT_OK){
+                Toast.makeText(this,data.getData().toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
